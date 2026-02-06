a7bcb4e7cf69bc0f9e468a98e8304fc18c294e0b
Mohamed YATTASSAYE <mohamedyattassaye35@icloud.com>
2026-02-06 11:43:09 +0100

    Swagger premier pas

--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,10 +1,17 @@
 package com.dauphine.blogger.controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@Tag(
+        name="Hello World API",
+        description = "My first Hello World endpoint"
+)
 public class HelloWorldController{
 
     @GetMapping("hello-world")
@@ -18,6 +25,11 @@ public class HelloWorldController{
     }
 
     @GetMapping("hello/{name}")
+    @Operation(
+            summary = "Hello by name endpoint",
+            description = "Returns 'Hello {name}' by path variable "
+    )
+    public String hello(@Parameter (description = "Name to greet") @PathVariable String name){
         return "Hello "+name;
     }
 
